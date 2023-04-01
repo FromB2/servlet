35c39aee29fad64258ae3e6afa66cd20db1dc775
Hwang joosun <santa2909@naver.com>
2023-04-02 00:44:18 +0900

    0402 ㅈㄴ조금했음;

--- src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -2,7 +2,6 @@ package hello.servlet.basic.response;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.servlet.basic.HelloData;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,9 +11,7 @@ import java.io.IOException;
 
 @WebServlet(name ="responseJsonServlet", urlPatterns ="/response-json")
 public class ResponseJsonServlet extends HttpServlet {
-
     private ObjectMapper objectMapper = new ObjectMapper();
-
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //Content-Type : application/json
--- src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -1,7 +1,15 @@
 package hello.servlet.domain.member;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    private static final MemberRepository instance = new MemberRepository();
+
+
 
 }
